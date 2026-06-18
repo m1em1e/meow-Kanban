6145d189ea40cc91af39ab3dba483ea57d05f6dc
mkdir <3044788388@qq.com>
2026-06-18 14:06:36 +0800

    feat: 生成制作登录注册页

---
+++ src/main/java/com/godotvillage/meowkanban/controller/PageController.java
@@ -0,0 +1,25 @@
+package com.godotvillage.meowkanban.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * 页面访问接口
+ *
+ * @author mkdir
+ * @since 2026/06/18 13:40
+ */
+@Controller
+public class PageController {
+
+	@GetMapping("/register")
+	public String register() {
+		return "register";
+	}
+
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
+
+}
