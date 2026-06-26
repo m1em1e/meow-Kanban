ee46d4ef7b5ee9659539714064652dbf8be170be
mkdir <3044788388@qq.com>
2026-06-26 17:11:51 +0800

    feat: 创建任务活动接口

--- src/main/java/com/godotvillage/meowkanban/service/ITaskActivityService.java
+++ src/main/java/com/godotvillage/meowkanban/service/ITaskActivityService.java
@@ -5,9 +5,12 @@ import com.godotvillage.meowkanban.common.result.PageResult;
 import com.godotvillage.meowkanban.domain.entity.TaskActivity;
 import com.godotvillage.meowkanban.domain.param.IdParam;
 import com.godotvillage.meowkanban.domain.param.RecentActivityParam;
+import com.godotvillage.meowkanban.domain.param.TaskActivityAddParam;
 import com.godotvillage.meowkanban.domain.vo.RecentActivityVO;
 
 public interface ITaskActivityService extends IService<TaskActivity> {
+
+	void addTaskActivity(TaskActivityAddParam param);
 
     PageResult<RecentActivityVO> getRecentActivity(RecentActivityParam param);
 
